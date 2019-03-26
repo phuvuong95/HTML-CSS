58f9b82fa1848cfd769df3ebfeafaef7d4f77750
NguyenPhuVuong <phuvuong95@gmail.com>
2019-03-26 22:25:14 +0700

    NpV

--- Unit09/js/bai4.js
+++ Unit09/js/bai4.js
@@ -1,5 +1,6 @@
 var button = document.getElementsByTagName("button");
 var content= document.getElementsByTagName("p");
+// var content1=document.getElementsByTagName("h3")
 for (var i = 0; i < content.length; i++) {
 	button[i].addEventListener("click", function(){
 		for (var j = 0; j < content.length; j++) {
@@ -8,3 +9,11 @@ for (var i = 0; i < content.length; i++) {
 		this.nextElementSibling.style.display="block";
 	});
 }
+// 	for (var i = 0; i < content1.length; i++) {
+// 	button[i].addEventListener("click", function(){
+// 		for (var j = 0; j < content1.length; j++) {
+// 			content1[j].style.display = "none";
+// 		}
+// 		this.nextElementSibling.style.display="block";
+// 	});
+// }
